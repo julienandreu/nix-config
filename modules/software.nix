3584f01ef14f44fe8f43f61525f2c8a1c7e367b7
Julien Andreu <julienandreu@me.com>
2025-12-15 17:34:00 -0500

    feat: easy onboarding

--- modules/software.nix
+++ modules/software.nix
@@ -51,48 +51,6 @@
     enable = true;
   };
 
-  # Ghostty configuration - inlined in Nix
-  xdg.configFile."ghostty/config".text = ''
-    # Ghostty Configuration with Catppuccin Mocha Theme
-
-    theme = catppuccin-mocha
-
-    font-family = MesloLGL Nerd Font Mono
-    font-size = 12
-    font-feature = calt
-    font-feature = liga
-
-    window-padding-x = 10
-    window-padding-y = 10
-    window-theme = dark
-
-    cursor-style = block
-    cursor-style-blink = true
-
-    shell-integration = zsh
-
-    background = "#1e1e2e"
-    foreground = "#cdd6f4"
-    cursor = "#f5e0dc"
-    selection-background = "#313244"
-    color0 = "#45475a"
-    color1 = "#f38ba8"
-    color2 = "#a6e3a1"
-    color3 = "#f9e2af"
-    color4 = "#89b4fa"
-    color5 = "#f5c2e7"
-    color6 = "#94e2d5"
-    color7 = "#bac2de"
-    color8 = "#585b70"
-    color9 = "#f38ba8"
-    color10 = "#a6e3a1"
-    color11 = "#f9e2af"
-    color12 = "#89b4fa"
-    color13 = "#f5c2e7"
-    color14 = "#94e2d5"
-    color15 = "#a6adc8"
-  '';
-
   # Cursor Editor configuration - inlined in Nix
   # Cursor uses VS Code's settings.json format
   # Path: ~/Library/Application Support/Cursor/User/settings.json
