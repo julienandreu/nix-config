a71bbd9d03a6803a3f61710f890a9dbc8c3f9d89
Julien Andreu <julienandreu@me.com>
2025-12-17 16:36:07 -0500

    fix: remove conflicting manual starship palette definition
    - Remove manual palettes definition that conflicts with catppuccin.starship module
    - Let catppuccin.starship module handle palette colors automatically
    - Keep custom format and other starship settings
    - Fixes error: conflicting definition values for palette colors

--- modules/theme.nix
+++ modules/theme.nix
@@ -18,36 +18,8 @@
 
       format = "[ ](surface0)$os$username[](bg:surface0 fg:base)$directory[](fg:base bg:green)$git_branch$git_status[](fg:green bg:teal)$nodejs$rust$python$golang[](fg:teal bg:peach)$time[](fg:peach bg:crust)$cmd_duration[](fg:crust)$line_break$character";
 
+      # Palette is set by catppuccin.starship module
       palette = "catppuccin_${catppuccinFlavor}";
-
-      palettes."catppuccin_${catppuccinFlavor}" = {
-        rosewater = "#f5e0dc";
-        flamingo = "#f2cdcd";
-        pink = "#f5c2e7";
-        orange = "#cba6f7";
-        red = "#f38ba8";
-        maroon = "#eba0ac";
-        peach = "#fab387";
-        yellow = "#f9e2af";
-        green = "#a6e3a1";
-        teal = "#94e2d5";
-        sky = "#89dceb";
-        sapphire = "#74c7ec";
-        blue = "#89b4fa";
-        lavender = "#b4befe";
-        text = "#cdd6f4";
-        subtext1 = "#bac2de";
-        subtext0 = "#a6adc8";
-        overlay2 = "#9399b2";
-        overlay1 = "#7f849c";
-        overlay0 = "#6c7086";
-        surface2 = "#585b70";
-        surface1 = "#45475a";
-        surface0 = "#313244";
-        base = "#1e1e2e";
-        mantle = "#181825";
-        crust = "#11111b";
-      };
 
       os = {
         disabled = false;
